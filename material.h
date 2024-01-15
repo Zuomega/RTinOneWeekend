71db1349421eb4c1412afb08fd2a1f7d5a60b992
Zuomega <25094987+Zuomega@users.noreply.github.com>
2024-01-15 22:10:12 +0800

    Done section 10.

--- material.h
+++ material.h
@@ -45,19 +45,21 @@ color albedo;
 
 class metal : public material{
 public:
-    metal(const color& a) : albedo(a) {}
+    metal(const color& a, FLOAT_FORMAT f) : albedo(a), fuzz(f<1 ? f : 1) {}
+    // a: albedo, f: fuzziness
 
     bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
     const override{
         //vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
         vec3 reflected = reflect((r_in.direction()), rec.normal);
-        scattered = ray(rec.p, reflected);
+        scattered = ray(rec.p, reflected + fuzz * random_unit_vector());
         attenuation = albedo;
-        return true;
+        return (dot(scattered.direction(), rec.normal) > 0);
     }
 
 private:
     color albedo;
+    FLOAT_FORMAT fuzz;
 };
 
 #endif
